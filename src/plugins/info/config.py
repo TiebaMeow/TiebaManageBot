b0d7e1d5388704b3dda7657d35dd8bc31715a2c4
mxcoras <2382413024@qq.com>
2025-11-30 01:55:51 +0800

    fix: annotation

--- src/plugins/info/config.py
+++ src/plugins/info/config.py
@@ -5,7 +5,7 @@ class Config(BaseModel):
     """Plugin Config Here"""
 
     ignore_users: list[int] = []
-    checkout_tieba = (
+    checkout_tieba: str = (
         "原神,原神内鬼,崩坏三,崩坏3rd,崩坏星穹铁道,星穹铁道内鬼,mihoyo,新mihoyo,尘白禁区,ml游戏,有男不玩ml,"
         "千年之旅,有男偷玩,二游笑话,dinner笑话,明日方舟,明日方舟内鬼,明日方舟dl,明日方舟pl,淋日方舟,"
         "血狼破军,快乐雪花,半壁江山雪之下,碧蓝航线,碧蓝航线2,异色格,赤色中轴,少女前线,少女前线2,"
