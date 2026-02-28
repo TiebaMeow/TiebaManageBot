3614883d53b6c4a46b6ae765f5b39ba8d99225fb
mxcoras <2382413024@qq.com>
2026-02-28 14:14:53 +0800

    fix: 修复消费者初始化逻辑，确保在启动前进行正确检查

--- src/addons/executor/__init__.py
+++ src/addons/executor/__init__.py
@@ -9,28 +9,32 @@ from .consumer import Consumer
 
 driver = get_driver()
 
-_consumer = Consumer()
+_consumer: Consumer | None = None
 
-task: asyncio.Task | None = None
+executor_task: asyncio.Task | None = None
 
 
-async def _run_consumer():
+async def __run_consumer():
+    if _consumer is None:
+        raise RuntimeError("Consumer not initialized")
     async with _consumer:
         await _consumer.run()
 
 
 @driver.on_bot_connect
 async def start_consumer():
-    global task
-    if task is None:
+    global executor_task, _consumer
+    if executor_task is None:
         log.info("Starting executor consumer...")
-        task = asyncio.create_task(_run_consumer())
+        _consumer = Consumer()
+        executor_task = asyncio.create_task(__run_consumer())
 
 
 @driver.on_shutdown
 async def stop_consumer():
     log.info("Stopping executor consumer...")
-    _consumer.stop()
-    if task:
-        await task
+    if _consumer:
+        _consumer.stop()
+    if executor_task:
+        await executor_task
     log.info("Executor consumer stopped.")
